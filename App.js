b1c11c070bb0ae746323b7c74022227c94a35096
남형석 <azerc@MacMini.local>
2023-07-03 00:30:20 +0900

    1. first step

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useState, useEffect, useCallback } from "react";
+import { Text, Image, View } from "react-native";
+
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset, useAssets } from "expo-asset";
+
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [ready, setReady] = useState(false);
+  // const [assets] = useAssets([require("./assets/splash_image.png")]);
+  // const [loaded] = Font.useFonts(Ionicons.font);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    async function prepare() {
+      try {
+        const fonts = await loadFonts([Ionicons.font]);
+        const images = await loadImages([require("./assets/splash_image.png")]);
+        await Promise.all([...fonts, ...images]);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        setReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  if (!ready) {
+    return null;
+  }
+  if (ready) {
+    SplashScreen.hideAsync();
+    return (
+      <NavigationContainer>
+        <Tabs />
+      </NavigationContainer>
+    );
+  }
+}
---
+++ navigation/Tabs.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Movies from "../screens/Movies";
+import Tv from "../screens/Tv";
+import Search from "../screens/Search";
+import { Text, View } from "react-native";
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => (
+  <Tab.Navigator
+    screenOptions={{
+      tabBarStyle: { backgroundColor: "tomato" },
+      tabBarActiveTintColor: "red",
+      tabBarInactiveTintColor: "purple",
+      headerTitleStyle: { color: "grey" },
+      headerTitleAlign: "center",
+      headerRight: () => (
+        <View>
+          <Text>Hello</Text>
+        </View>
+      ),
+    }}
+  >
+    <Tab.Screen name="Movies" component={Movies} />
+    <Tab.Screen name="Tv" component={Tv} />
+    <Tab.Screen name="Search" component={Search} />
+  </Tab.Navigator>
+);
+
+export default Tabs;
